86ed80852e1c5fe87bb332733fbb19cc8890b6e5
DESKTOP-KBC3GUP\1 <as1095051785@sina.com>
2018-06-21 14:52:47 +0800

    初始化项目

---
+++ gitDemo/src/gitDemo/Demo01.java
@@ -0,0 +1,14 @@
+package gitDemo;
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//123
+		System.out.println("hahah");
+		//111
+		//222
+		System.out.println("大佬666！");
+	}
+
+}
